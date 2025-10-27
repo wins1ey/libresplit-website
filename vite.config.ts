4a94ea213dbcbcd6376d6df35fd7726c1a42b6d0
JackTench <79285604+JackTench@users.noreply.github.com>
2025-10-27 22:25:34 +0000

    Add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/libresplit-website/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
