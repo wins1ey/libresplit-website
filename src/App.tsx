3542f260229400088e3c59992748c751cb582c3f
JackTench <79285604+JackTench@users.noreply.github.com>
2025-10-27 12:21:58 +0000

    Install prettier and enforce code formatting across the repo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
+        <a href="https://vite.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -24,7 +22,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
