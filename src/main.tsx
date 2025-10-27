01cc6222c93957bcba357f5a5a3891edf5d304f8
JackTench <79285604+JackTench@users.noreply.github.com>
2025-10-27 20:35:06 +0000

    Install next-themes and wrap with ThemeProvider.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import { StrictMode } from "react";
 
 import App from "./app";
 import "./index.css";
+import { ThemeProvider } from "next-themes";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>,
 );
