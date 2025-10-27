1a254f5b389228ccb6a9a5a487dd32bdfefb9ada
JackTench <79285604+JackTench@users.noreply.github.com>
2025-10-27 17:52:24 +0000

    Install react-router.

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { StrictMode } from "react";
 import App from "./App.tsx";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
