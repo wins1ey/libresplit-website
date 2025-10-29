e0483631cf955f962d42a9dc3edcce551bbc626a
JackTench <79285604+JackTench@users.noreply.github.com>
2025-10-29 00:24:48 +0000

    Change loading bar to green

--- src/components/libresplit/AppLoading.tsx
+++ src/components/libresplit/AppLoading.tsx
@@ -3,7 +3,7 @@ import { BarLoader } from "react-spinners";
 export function AppLoading() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <BarLoader />
+      <BarLoader color={"#00ff00"} />
     </div>
   );
 }
