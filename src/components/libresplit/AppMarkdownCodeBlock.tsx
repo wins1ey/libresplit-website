475418c6961e7d1e3e29f85af11dd84aea13c217
JackTench <79285604+JackTench@users.noreply.github.com>
2025-10-29 23:14:46 +0000

    Redesign code blocks

--- src/components/libresplit/AppMarkdownCodeBlock.tsx
+++ src/components/libresplit/AppMarkdownCodeBlock.tsx
@@ -1,25 +1,28 @@
+import { Card, CardContent } from "../ui/card";
 import { CodeBlock } from "react-code-block";
 
 export type AppMarkdownCodeBlockProps = {
   code: string;
   language?: string;
-  className?: string;
 };
 
 export function AppMarkdownCodeBlock({
   code,
   language,
-  className,
 }: AppMarkdownCodeBlockProps) {
   return (
-    <div className={className}>
-      <CodeBlock code={code} language={language ?? "text"}>
-        <CodeBlock.Code>
-          <CodeBlock.LineContent>
-            <CodeBlock.Token />
-          </CodeBlock.LineContent>
-        </CodeBlock.Code>
-      </CodeBlock>
+    <div className="w-fit px-2">
+      <Card className="w-fit">
+        <CardContent className="w-fit">
+          <CodeBlock code={code} language={language ?? "text"}>
+            <CodeBlock.Code>
+              <CodeBlock.LineContent>
+                <CodeBlock.Token />
+              </CodeBlock.LineContent>
+            </CodeBlock.Code>
+          </CodeBlock>
+        </CardContent>
+      </Card>
     </div>
   );
 }
